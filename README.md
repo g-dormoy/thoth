5bd7b6b18280837b52de16efed092499e78f0379
Guillaume Dormoy <g.dormoy@pm.me>
2019-06-02 22:50:55 +0200

    add method to get services

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # thoth
 
 Is a service mesh
+
+## Add a service
+
+```bash
+curl -X POST http://localhost:4242/add -H "Content-Type: application/json" --data '{"name": "service1", .....}'
+```
+
+## Get a service
+
+```bash
+curl http://localhost:4242/get?name=service1
+```
--- main.go
+++ main.go
@@ -7,13 +7,50 @@ import (
 	"sync"
 )
 
+// ServiceCollection represent a collection for Services
+type ServiceCollection []Service
+
+// Filter return a new collection based of the filtering function result
+func (sc ServiceCollection) Filter(ff func(Service) bool) []Service {
+	var nsc []Service
+
+	for _, service := range sc {
+		if ff(service) == true {
+			nsc = append(nsc, service)
+		}
+	}
+
+	return nsc
+}
+
 // Service Struct represent a service
 type Service struct {
 	Name string `json:"name"`
 	IP   string `json:"ip"`
 	Port string `json:"port"`
-	Desc string `json:"desc"`
+	Desc string `json:"desc,omitempty"`
 	Type string `json:"type"`
+}
+
+// getHandler handler http request to add new service
+func getHandler(mutex *sync.Mutex, services ServiceCollection) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// If not a PUT request, return an error
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		// Checkin the URL parameters
+		// TO-DO Add clarity when name param is missing
+		name := r.URL.Query().Get("name")
+		if name != "" {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+		s := services.Filter(func(service Service) bool { return service.Name == name })
+		_ = json.NewEncoder(w).Encode(s)
+	}
 }
 
 // addHandler handler http request to add new service
@@ -36,6 +73,8 @@ func addHandler(mutex *sync.Mutex, services []Service) func(w http.ResponseWrite
 		mutex.Lock()
 		services = append(services, service)
 		mutex.Unlock()
+
+		w.WriteHeader(http.StatusCreated)
 	}
 }
 
@@ -43,5 +82,6 @@ func main() {
 	var services []Service
 	mutex := &sync.Mutex{}
 	http.HandleFunc("/add", addHandler(mutex, services))
+	http.HandleFunc("/get", getHandler(mutex, services))
 	http.ListenAndServe(":4242", nil)
 }
